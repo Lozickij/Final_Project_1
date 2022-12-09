aa70de91d5377672ae7090aa92110757ef8bc65b
АЛЕКСАНДР ЛОЗИЦКИЙ <avtolik2015@outlook.com>
2022-12-09 19:00:17 +0300

    Program

--- Program folder/Program.cs
+++ Program folder/Program.cs
@@ -3,8 +3,8 @@
 [“1234”, “1567”, “-2”, “computer science”] → [“-2”]
 [“Russia”, “Denmark”, “Kazan”] → []*/
 
-//string[] array1 = new string[] {"Hello", "2", "world", ":-)"};
-string[] array1 = new string[] {"1234", "1567", "-2", "computer science"};
+string[] array1 = new string[] {"Hello", "2", "world", ":-)"};
+//string[] array1 = new string[] {"1234", "1567", "-2", "computer science"};
 //string[] array1 = new string[] {"Russia", "Denmark", "Kazan"};
 string[] array2 = new string[array1.Length];
 void CreateAnotherArray(string[] array1, string[] array2)
@@ -21,13 +21,14 @@ void CreateAnotherArray(string[] array1, string[] array2)
 }
 void ShowArray(string[] array)
 {
-    Console.Write(" ");
+    Console.Write("[");
     for (int i = 0; i < array.Length; i++)
     {
         Console.Write($"{array[i]} ");
     }
-    Console.WriteLine();
+    Console.Write("]");
 }
 ShowArray(array1);
 CreateAnotherArray(array1, array2);
+Console.Write(" -> ");
 ShowArray(array2);
